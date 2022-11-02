2e78d9fa0b7ba10d9521e5e3cf8c2da6dae66536
victordaniel21 <victordaniel.dev@gmail.com>
2022-11-02 08:12:41 +0800

    remember ln 22 is not a goood idea

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func printA(a string) {
 	fmt.Println(a)
@@ -8,7 +11,15 @@ func printA(a string) {
 func main() {
 	// the main function itself is a go-routine; run a very lightweight thread
 
-	go printA("Print a letter ''A'' to the monitor") // add "go" will tell the compiler to execute in its own go-routine
+	words := []string{
+		"alpha", "beta", "delta", "epsilon", "gamma", "pi", "theta", "zeta", "omega",
+	}
+
+	for i, word := range words {
+		go printA(fmt.Sprintf("no. %d: %s", i, word))
+	}
+
+	time.Sleep(1 * time.Second) // using this method is not a good idea, change to wait group instead
 
 	printA("Print a letter ''B'' to the monitor")
 }
